8ad82f9855d590e00202e633c94c19b8a85e5ddc
Abhishek Singh <73503674+abhivlogsingh@users.noreply.github.com>
2023-03-13 13:36:17 +0530

    Add files via upload

---
+++ abhi portfolio.main/abhishek-main/js/autoTypingAnimation.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: ["<span> Hi, I am Abhishek Singh </span> Working in Web Development , Enthusiastic Technology with a demonstrated history of learning in the information technology and services industry. Skilled in WEB Tech, JAVA , Web development with multiple tools and technologies . B.Tech focused in Computer Science from Rama University, Kanpur "],
+      typeSpeed: 01,
+      contentType: 'html'
+      
+    });
+  }, 1000);
